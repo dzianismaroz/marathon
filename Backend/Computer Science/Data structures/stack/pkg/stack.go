c5e17ca826a3be50a37e52cf003bc0d5e27e79e8
Dzianis Maroz <d.moroz@atomgroup.io>
2024-12-10 11:45:37 +0300

    Konstanteen, what do you think ?

--- Backend/Computer Science/Data structures/stack/pkg/stack.go
+++ Backend/Computer Science/Data structures/stack/pkg/stack.go
@@ -3,16 +3,18 @@ package stack
 import "sync"
 
 type (
-	el[T any] struct {
-		val  T
-		next *el[T]
+
+	// element is wrapper to hold stored value and link to next element.
+	element[T any] struct {
+		value T
+		next  *element[T]
 	}
 
 	// LIFO implementation.
 	Stack[T any] struct {
 		mu     sync.RWMutex
-		first  *el[T]
-		length uint // optimal way for Stack to track its length.
+		first  *element[T] // head of Stask.
+		length uint        // optimal way for Stack to track its length.
 	}
 )
 
@@ -22,15 +24,11 @@ func NewStack[T any]() *Stack[T] {
 }
 
 // Asymptomatic : O(1)
-func (s *Stack[T]) Push(elem T) {
+func (s *Stack[T]) Push(val T) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	s.first = &el[T]{ // O(1)
-		val:  elem,
-		next: s.first,
-	}
-
+	s.first = &element[T]{value: val, next: s.first}
 	s.length++
 }
 
@@ -45,9 +43,7 @@ func (s *Stack[T]) Pop() (T, bool) {
 		return result, false
 	}
 
-	result = s.first.val
-	s.first = s.first.next
-
+	result, s.first = s.first.value, s.first.next
 	s.length--
 
 	return result, true
--- Backend/Computer Science/Data structures/tree/tree/tree.go
+++ Backend/Computer Science/Data structures/tree/tree/tree.go
@@ -1,15 +1,33 @@
 package tree
 
-type Tree[T any] struct {
-	val   T
-	left  *Tree[T]
-	right *Tree[T]
-}
+type (
+	node[T comparable] struct {
+		val   T
+		left  *node[T]
+		right *node[T]
+	}
 
-func New[T any]() *Tree[T] {
+	Tree[T comparable] struct {
+		root *node[T]
+	}
+)
+
+func New[T comparable]() *Tree[T] {
 	return &Tree[T]{}
 }
 
-func (t *Tree[T]) Traverse() {
+func (t *Tree[T]) Add(elem T) {
+	if t.root == nil {
+		t.root = &node[T]{val: elem}
+
+		return
+	}
+
+}
+
+func (t *Tree[T]) addRecursive(n *node[T], elem T) {
+	if n.val < elem {
+
+	}
 
 }
